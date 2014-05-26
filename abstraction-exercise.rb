70be43035830ac7f59f77916efa2e88e06711f0f
Dave Astels <dastels@icloud.com>
2014-05-26 09:49:37 -0500

    abstraction exercise

---
+++ abstraction-exercise.rb
@@ -0,0 +1,9 @@
+# Add to this and write client code to add labels and print out each label, capitalized.
+# Do it without accessing instance vars from outside the object.
+
+
+class  Node
+  def initialize
+    @labels = []
+  end
+end
