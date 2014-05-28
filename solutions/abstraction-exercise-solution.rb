b5d9c76388366a4ef0a803cc6da075e3c8d50ac7
Dave Astels <dastels@icloud.com>
2014-05-28 15:13:55 -0500

    more examples

--- solutions/abstraction-exercise-solution.rb
+++ solutions/abstraction-exercise-solution.rb
@@ -20,3 +20,5 @@ n.add_label("movie")
 n.add_label("action")
 n.add_label("FEATURE")
 n.with_each_label {|l| puts l.capitalize}
+n.with_each_label {|l| puts l.downcase}
+n.with_each_label {|l| puts l.upcase}
